155baaa95c217f36fbabd8f62174e011bb84a0da
Lolfoollor <L_Z_F47@hotmail.com>
2024-09-16 23:44:59 +0800

    Add Springboot v3.3.3 and CI
    Github Actions workflow is added to build and test
    every push and pull request. This is to ensure that
    regression bug is found and eliminated.

---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/GarangGuniBackendApplication.java
@@ -0,0 +1,11 @@
+package sg.edu.ntu.garang_guni_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarangGuniBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GarangGuniBackendApplication.class, args);
+    }
+}
