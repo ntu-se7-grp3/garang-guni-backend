9c9b1828deb2cb9cb463d725d66e9cf87ce77a2b
Lolfoollor <L_Z_F47@hotmail.com>
2024-09-19 07:33:51 +0800

    test: add unit tests for ImageUtils

---
+++ src/test/java/sg/edu/ntu/garang_guni_backend/utils/ImageUtilsTest.java
@@ -0,0 +1,70 @@
+package sg.edu.ntu.garang_guni_backend.utils;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.multipart.MultipartFile;
+
+@SpringBootTest
+class ImageUtilsTest {
+
+    @Mock
+    private MultipartFile file;
+
+    @DisplayName("Compress and Decompress Image - Success")
+    @Test
+    void compressAndDecompressImageTest() throws Exception {
+        byte[] originalData = "This is some test image data".getBytes();
+        byte[] compressedData = ImageUtils.compressImage(originalData);
+        byte[] decompressedData = ImageUtils.decompressImage(compressedData);
+        assertArrayEquals(originalData, decompressedData);
+    }
+
+    @DisplayName("isImage - Success")
+    @Test
+    void IsImageTest() {
+        when(file.getOriginalFilename()).thenReturn("test_image.jpg");
+        assertTrue(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image.jpeg");
+        assertTrue(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image.png");
+        assertTrue(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image.gif");
+        assertTrue(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image.bmp");
+        assertTrue(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image.test.png");
+        assertTrue(ImageUtils.isImage(file));
+
+    } 
+
+    @DisplayName("isImage - Non Existant File or File Name")
+    @Test
+    void IsImageNonExistantFileOrFileNameTest() {
+        assertFalse(ImageUtils.isImage(null));
+
+        when(file.getOriginalFilename()).thenReturn(null);
+        assertFalse(ImageUtils.isImage(file));
+    } 
+
+    @DisplayName("isImage - Invalid Extension")
+    @Test
+    void IsImageInvalidExtensionTest() {
+        when(file.getOriginalFilename()).thenReturn("test_image.apng");
+        assertFalse(ImageUtils.isImage(file));
+
+        when(file.getOriginalFilename()).thenReturn("test_image");
+        assertFalse(ImageUtils.isImage(file));
+    } 
+}
