f02448c228bdfa914acdbfeb7cacb48fe9b6ce42
Lolfoollor <L_Z_F47@hotmail.com>
2024-09-20 07:40:02 +0800

    feat: add Item controller

---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/controllers/ItemController.java
@@ -0,0 +1,53 @@
+package sg.edu.ntu.garang_guni_backend.controllers;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sg.edu.ntu.garang_guni_backend.entities.Item;
+import sg.edu.ntu.garang_guni_backend.services.ItemService;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    @SuppressFBWarnings("EI_EXPOSE_REP2")
+    private final ItemService itemService;
+
+    public ItemController(@Qualifier("itemServiceImpl") ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping({ "", "/" })
+    public ResponseEntity<Item> createItem(@RequestBody Item newItem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(itemService.createItem(newItem));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable UUID id) {
+        Item selecetedItem = itemService.getItemById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(selecetedItem);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> updateItem(@PathVariable UUID id, 
+                @RequestBody Item updatedItem) {
+        Item selectedItem = itemService.updateItem(id, updatedItem);
+        return ResponseEntity.status(HttpStatus.OK).body(selectedItem);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Item> deleteImage(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                            .body(itemService.deleteImage(id));
+    }
+}
