0bca891d5cf9ab217b37313538c7c8f5042dd94b
Lolfoollor <L_Z_F47@hotmail.com>
2024-09-20 08:26:54 +0800

    feat: add general exceptions into global exception handler

--- src/main/java/sg/edu/ntu/garang_guni_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/sg/edu/ntu/garang_guni_backend/exceptions/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package sg.edu.ntu.garang_guni_backend.exceptions;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -36,5 +41,35 @@ public class GlobalExceptionHandler {
         }
         ErrorResponse errorResponse = new ErrorResponse(sb.toString(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        String errorMsg = ex.getMessage();
+        //
+        String re = "Detail: ([^]]+)";
+        Pattern pattern = Pattern.compile(re);
+        Matcher matcher = pattern.matcher(errorMsg);
+        String detailMsg = "";
+        if (matcher.find()) {
+            detailMsg = matcher.group(1).trim();
+        } else {
+            detailMsg = "Unknown unique constraint has been violated";
+        }
+
+        ErrorResponse errorMessage = new ErrorResponse("A data integrity error occurred: " + detailMsg, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Malformed JSON request", LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Something went wrong", LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
