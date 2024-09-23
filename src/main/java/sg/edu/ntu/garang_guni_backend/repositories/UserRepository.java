4ba0b5ea71b26459b0344771881a394bc1475ae5
asherchew <chinhao1996@hotmail.com>
2024-09-23 09:05:24 +0800

    Commit missing file UserRepository.java

---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package sg.edu.ntu.garang_guni_backend.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sg.edu.ntu.garang_guni_backend.entities.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
