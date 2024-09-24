bb475ffa8716d5db0f27058c7357b76c067b1dd3
Lolfoollor <L_Z_F47@hotmail.com>
2024-09-24 10:18:59 +0800

    feat: add location service

---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/services/LocationService.java
@@ -0,0 +1,17 @@
+package sg.edu.ntu.garang_guni_backend.services;
+
+import java.util.List;
+import java.util.UUID;
+import sg.edu.ntu.garang_guni_backend.entities.Location;
+
+public interface LocationService {
+    Location createLocation(Location location);
+
+    Location getLocationById(UUID locationId);
+
+    List<Location> getLocationsWithoutBooking();
+
+    Location updateLocation(UUID locationId, Location updatedLocatio);
+
+    Location deleteLocation(UUID locationId);
+}
---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/services/impls/LocationServiceImpl.java
@@ -0,0 +1,65 @@
+package sg.edu.ntu.garang_guni_backend.services.impls;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import sg.edu.ntu.garang_guni_backend.entities.Location;
+import sg.edu.ntu.garang_guni_backend.exceptions.location.LocationNotFoundException;
+import sg.edu.ntu.garang_guni_backend.repositories.LocationRepository;
+import sg.edu.ntu.garang_guni_backend.services.LocationService;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+    private LocationRepository locationRepository;
+
+    public LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    @Override
+    public Location createLocation(Location newLocation) {
+        return locationRepository.save(newLocation);
+    }
+
+    @Override
+    public Location getLocationById(UUID locationId) {
+        Location retrievedLocation = locationRepository.findById(locationId)
+                .orElseThrow(() -> new LocationNotFoundException(locationId));
+
+        return new Location(retrievedLocation);
+    }
+
+    @Override
+    public List<Location> getLocationsWithoutBooking() {
+        List<Location> locations = locationRepository.findAll()
+                                    .stream()
+                                    .map(Location::new)
+                                    .toList();
+            
+        locations.forEach(location -> location.setBooking(null));
+        return locations;
+    }
+
+    @Override
+    public Location updateLocation(UUID locationId, Location updatedLocation) {
+        Location locationToUpdate = locationRepository.findById(locationId)
+                .orElseThrow(() -> new LocationNotFoundException(locationId));
+
+        locationToUpdate.setLocationName(updatedLocation.getLocationName());
+        locationToUpdate.setLocationAddress(updatedLocation.getLocationAddress());
+        locationToUpdate.setLatitude(updatedLocation.getLatitude());
+        locationToUpdate.setLongitude(updatedLocation.getLongitude());
+
+        return new Location(locationToUpdate);
+    }
+
+    @Override
+    public Location deleteLocation(UUID locationId) {
+        Location locationToDelete = locationRepository.findById(locationId)
+                .orElseThrow(() -> new LocationNotFoundException(locationId));
+        locationRepository.deleteById(locationId);
+
+        return locationToDelete;
+    }
+    
+}
