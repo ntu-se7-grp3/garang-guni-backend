8dcce9680c389a09ce5e6c325b33d7fc14b3e145
ttan-ttan <yong4425@live.com>
2024-09-26 13:17:34 +0800

    updated availabiliry Repo

--- src/main/java/sg/edu/ntu/garang_guni_backend/repositories/AvailabilityRepository.java
+++ src/main/java/sg/edu/ntu/garang_guni_backend/repositories/AvailabilityRepository.java
@@ -1,17 +1,16 @@
 package sg.edu.ntu.garang_guni_backend.repositories;
 
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import sg.edu.ntu.garang_guni_backend.entities.Availability;
 
 public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
     List<Availability> findByAvailableDateAndLocation(LocalDate availableDate, String location);
 
-    List<Availability> findByScrapDealer_ScrapDealerIdAndAvailableDate(UUID scrapDealerId, LocalDate availableDate);
+    List<Availability> findByScrapDealer_ScrapDealerIdAndAvailableDate(
+        UUID scrapDealerId, LocalDate availableDate);
 
     List<Availability> findByLocation(String location);
 
