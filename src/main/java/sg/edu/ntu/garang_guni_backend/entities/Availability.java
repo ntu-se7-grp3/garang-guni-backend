e666ffe0b339a02308ab84aa47fff31d8a0b533c
ttan-ttan <yong4425@live.com>
2024-09-26 21:23:18 +0800

    seperated location and availability

--- src/main/java/sg/edu/ntu/garang_guni_backend/entities/Availability.java
+++ src/main/java/sg/edu/ntu/garang_guni_backend/entities/Availability.java
@@ -1,17 +1,9 @@
 package sg.edu.ntu.garang_guni_backend.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,41 +13,32 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "scrap_dealer_Availability")
+@Table(name = "availability")
 public class Availability {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Location is required")
-    @Size(max = 50, message = "Location should not exceed 50 characters")
-    private String location;
-
     @NotNull(message = "Available date is required")
     @FutureOrPresent(message = "Available date must be today or in the future")
     private LocalDate availableDate;
 
+    // Many-to-One relationship with Location
+    @ManyToOne
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    // Many-to-One relationship with ScrapDealer
     @ManyToOne
     @JoinColumn(name = "scrap_dealer_id")
     @JsonBackReference
     private ScrapDealer scrapDealer;
 
-    public Availability(
-        Long id, String location, LocalDate availableDate, ScrapDealer scrapDealer) {
-        this.id = id;
-        this.location = location;
+    // Constructor to initialize all fields
+    public Availability(LocalDate availableDate, Location location, ScrapDealer scrapDealer) {
         this.availableDate = availableDate;
-        this.scrapDealer = new ScrapDealer(scrapDealer);  // Defensive copy
-    }
-
-    // Getter with defensive copy
-    public ScrapDealer getScrapDealer() {
-        return new ScrapDealer(scrapDealer);
-    }
-
-    // Setter with defensive copy
-    public void setScrapDealer(ScrapDealer scrapDealer) {
-        this.scrapDealer = new ScrapDealer(scrapDealer);
+        this.location = location;
+        this.scrapDealer = scrapDealer;
     }
 }
---
+++ src/main/java/sg/edu/ntu/garang_guni_backend/entities/Location.java
@@ -0,0 +1,39 @@
+package sg.edu.ntu.garang_guni_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Location name is required")
+    @Size(max = 50, message = "Location name should not exceed 50 characters")
+    private String name;
+
+    @NotNull(message = "Latitude is required")
+    @DecimalMin(value = "-90.0", message = "Latitude must be between -90 and 90")
+    @DecimalMax(value = "90.0", message = "Latitude must be between -90 and 90")
+    private Double latitude;
+
+    @NotNull(message = "Longitude is required")
+    @DecimalMin(value = "-180.0", message = "Longitude must be between -180 and 180")
+    @DecimalMax(value = "180.0", message = "Longitude must be between -180 and 180")
+    private Double longitude;
+}
